0ef5e00c8b8b2e9fa2614500a6abcc266665fcd6
이지숙 <110869913+Jisook93@users.noreply.github.com>
2023-06-20 11:22:57 +0900

    Time: 168 ms (19.01%), Space: 42 MB (86.85%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const outputIndex = nums.indexOf(target - nums[i]);
+    
+    if (outputIndex !== -1  && outputIndex !== i) {
+      return [i, outputIndex];
+    } 
+  }
+};
