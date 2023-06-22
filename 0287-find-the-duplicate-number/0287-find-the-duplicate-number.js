89e567865607d9c947598980100fa55b1437022b
이지숙 <110869913+Jisook93@users.noreply.github.com>
2023-06-22 11:51:17 +0900

    Time: 137 ms (24.79%), Space: 70 MB (5.04%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findDuplicate = function (nums) {
+  const map = new Map();
+
+  nums.forEach((num) => {
+    map.has(num) ? map.set(num, 2) : map.set(num, 1);
+  });
+
+  for (let [key, value] of map) {
+    if (value === 2) return key;
+  }
+};
