f45ce90b8bcbdafe3ee1ff542e4a73706b392af9
이지숙 <110869913+Jisook93@users.noreply.github.com>
2023-06-26 11:14:14 +0900

    Time: 67 ms (26.09%), Space: 41.9 MB (92.56%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+  if (!nums.length) return init;
+
+  let result;
+  nums.forEach((num) => {
+    result = fn(init, num);
+    init = result;
+  });
+
+  return result;
+};
