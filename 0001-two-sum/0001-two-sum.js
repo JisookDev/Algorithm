dde8f22471fe98d27bfee456c7e1f759a6bd9986
이지숙 <110869913+JisookDev@users.noreply.github.com>
2024-08-29 12:08:45 +0900

    Time: 202 ms (5.06%), Space: 51.8 MB (14.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,12 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    const outputIndex = nums.indexOf(target - nums[i]);
+var twoSum = function(nums, target) {
+    //객체로 만듦. {index: value} 일차적으로 전체 순회하면서 만들고, 배열.indexOf(target - 객체[i]) 한 값이 있으면 반환.
+    const obj = {};
+    nums.map((num, index) => {
+        obj[index] = num;
+    });
     
-    if (outputIndex !== -1  && outputIndex !== i) {
-      return [i, outputIndex];
-    } 
-  }
+    for (let i = 0; i < nums.length; i++) {
+        const setNumber = target - obj[i];
+        const setNumIndex = nums.indexOf(target - obj[i]);
+        
+        if (nums.includes(setNumber) && setNumIndex !== i) {
+            return [i, setNumIndex];
+        }
+    }
+    
+    return false;
+
 };
