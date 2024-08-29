8c7f83f7a89026b99aa2abbcd8dd6e25b7c3a92c
이지숙 <110869913+JisookDev@users.noreply.github.com>
2024-08-29 15:40:36 +0900

    Time: 70 ms (33.66%), Space: 52.5 MB (75.48%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.js
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.js
@@ -10,22 +10,21 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-
-function TreeNode(val, left, right) {
-    this.val = (val===undefined ? 0 : val)
-    this.left = (left===undefined ? null : left)
-    this.right = (right===undefined ? null : right)
- }
-
 var sortedArrayToBST = function(nums) {
-    if(nums.length === 0) return null;
-    if(nums.length === 1) return new TreeNode(nums[0]);
+    //중간값 잡아서 왼쪽 정렬. 오른쪽 정렬. -> 왼쪽 재귀. 오른쪽 재귀
+    //탈출 조건 만들기
+    if (nums.length === 1) {
+        return new TreeNode(nums[0]);
+    }
     
-    const middleIndex =  Math.floor(nums.length/2);
-    const root = new TreeNode(nums[middleIndex]);
-    root.left = sortedArrayToBST(nums.slice(0, middleIndex));
-    root.right = sortedArrayToBST(nums.slice(middleIndex+1, nums.length));
+    if (nums.length === 0) {
+        return null
+    }
     
-    return root;
+    let center = Math.floor(nums.length / 2);
+    const root = new TreeNode(nums[center]);
+    root.left = sortedArrayToBST(nums.slice(0,center));
+    root.right = sortedArrayToBST(nums.slice(center + 1));
+    
+    return root
 };
-
