0c5d2cf4f302f8840bd7e3c2263b060cedd15df3
이지숙 <110869913+JisookDev@users.noreply.github.com>
2024-09-05 07:56:05 +0900

    Time: 81 ms (25.65%), Space: 58.9 MB (64.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let left = 0;
-  let right = 1;
-  let max_income = 0;
-
-  while (right < prices.length) {
-    if (prices[left] < prices[right]) {
-      max_income = Math.max(max_income, prices[right] - prices[left]);
-    } else {
-      left = right;
+    let maxProfit = 0;
+    let minPrice = prices[0];
+    
+    for(let i=1; i<prices.length; i++) {
+        let sell = prices[i];
+        let profit = sell - minPrice;
+        
+        maxProfit = Math.max(maxProfit, profit);
+        
+        if(sell < minPrice) {
+            minPrice = sell;
+        }
     }
-    right += 1;
-  }
-  
-  return max_income;
+    return maxProfit;
 };
